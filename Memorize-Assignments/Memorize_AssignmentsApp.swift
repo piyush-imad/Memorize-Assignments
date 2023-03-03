3894c9651d19a8c9a2bbe074058ccad736ebde73
Piyush <piyush9401@gmail.com>
2023-03-03 20:26:48 +0530

    Initial Commit

---
+++ Memorize-Assignments/Memorize_AssignmentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_AssignmentsApp.swift
+//  Memorize-Assignments
+//
+//  Created by Piyush Sharma on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_AssignmentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
