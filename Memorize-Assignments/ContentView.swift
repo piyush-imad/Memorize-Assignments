3520eb62b81027085aa6c666800a281a58423450
Piyush <piyush9401@gmail.com>
2023-03-04 19:29:20 +0530

    Assignment 1 of course Stanford 193p

--- Memorize-Assignments/ContentView.swift
+++ Memorize-Assignments/ContentView.swift
@@ -8,14 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var emojis = ["✈️","🚚","🚕","🛫","🚛","🚙","🚘","🚀"]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Memorize!")
+                .font(.largeTitle)
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
+                    ForEach(emojis, id: \.self) { emoji in
+                        CardView(content: emoji)
+                    }.aspectRatio(2/3, contentMode: .fit)
+                }
+            }
+            HStack {
+                Spacer()
+                vehiclesTheme
+                Spacer()
+                christmasTheme
+                Spacer()
+                peopleTheme
+                Spacer()
+            }
+            .font(.body)
         }
         .padding()
+    }
+    
+    var vehiclesTheme: some View {
+        Button(action: {
+            emojis = ["✈️","🚚","🚕","🛫","🚛","🚙","🚘","🚀"].shuffled()
+        }, label: {
+            VStack {
+                Image(systemName: "car.fill")
+                Text("Vehicles")
+            }
+        })
+    }
+    
+    var christmasTheme: some View {
+        Button(action: {
+            emojis = ["✝️","⛪️","🎄","🎅🏻","🇨🇽","🤶🏼"].shuffled()
+        }, label: {
+            VStack {
+                Image(systemName: "tree.fill")
+                Text("Christmas")
+            }
+        })
+    }
+    
+    var peopleTheme: some View {
+        Button(action: {
+            emojis = ["👩🏻‍🦰","👨‍👩‍👦","🚶🏻‍♂️","🧍🏽‍♂️","👨‍👦‍👦","👬"].shuffled()
+        }, label: {
+            VStack {
+                Image(systemName: "person.fill")
+                Text("People")
+            }
+        })
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp = true
+    let content: String
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 5).foregroundColor(.red)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill().foregroundColor(.red)
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
